8fa1f14f83d75ede38bf2cc19a4d2a0e8a95719d
Melodi <62381002+Melodi17@users.noreply.github.com>
2023-12-13 12:36:54 +1000

    reloaded new stuff, mainly docs

---
+++ build/src/js/download.js
@@ -0,0 +1,7 @@
+function downloadbinary() {
+    window.location.href = "https://github.com/Melodi17/Point/releases/latest/download/Point.exe";
+}
+
+function visitsource() {
+    window.location.href = "https://github.com/Melodi17/Point/";
+}
